a31c6db5e083afc6ed2dba7973437e7be44bcb13
Zilraen <zilraen@gmail.com>
2014-10-27 14:09:48 +0200

    * fix to pass first 2 test cases in task medium 11 "Bender, a Depressed Robot"

--- medium11.py
+++ medium11.py
@@ -5,8 +5,13 @@ import sys, math
 
 cells = {"bender": "@", "booth": "$", "none": " ", "obtacle": "X#"}
 directions = {"SOUTH": (0, 1), "EAST": (1, 0), "NORTH": (0, -1), "WEST": (-1, 0)}
+movementPrio = ["SOUTH", "EAST", "NORTH", "WEST"]
+
+states = {"cannot_move": 0, "moved": 1, "succeed": 2}
+
 
 def GetCoord(signs, row, rowy):
+    #print >> sys.stderr, "searching: ", signs, " in ", row, " #", rowy
     result = []
     for sign in signs:
         if sign in row:
@@ -16,11 +21,14 @@ def GetCoord(signs, row, rowy):
     return result
     
 def tryMove(loc, direct, map):
-    newLoc = loc + direct
-    if map[newLoc[1], newLoc[0]] not in cells["obtacle"]:
-        return True
+    newloc = tuple(sum(t) for t in zip(loc, direct))
+    print >> sys.stderr, "trymove: ", loc, " in ", direct, ": ", newloc
+    if newloc in data["obtacle"]:
+        return (states["cannot_move"], loc)
+    elif newloc in data["booth"]:
+        return (states["succeed"], newloc)
     else:
-        return False
+        return (states["moved"], newloc)
 
 L, C = [int(i) for i in raw_input().split()]
 map = []
@@ -36,17 +44,30 @@ for i in xrange(L):
     print >> sys.stderr, row
     
     for cell in cells:
-        data[cell] += GetCoord(cell, row, i)
+        data[cell] += GetCoord(cells[cell], row, i)
     
     map.append(row)
     
 print >> sys.stderr, "data: ", data
 
-commands += "SOUTH"
-
-for direction in directions:
-    if tryMove()
+state = states["cannot_move"]
+while 1:
+    for movement in movementPrio:
+        state = tryMove(data["bender"][0], directions[movement], map)
+        if state[0] == states["moved"]:
+            data["bender"][0] = state[1]
+            commands.append(movement)
+            print >> sys.stderr, "move: ", movement
+            break
+        elif state[0] == states["succeed"]:
+            data["bender"][0] = state[1]
+            commands.append(movement)
+            print >> sys.stderr, "move: ", movement
+            break
+    if state[0] == states["succeed"]:
+        break
 # Write an action using print
 # To debug: print >> sys.stderr, "Debug messages..."
 
-print "/n".join(commands)
+for command in commands:
+    print command
